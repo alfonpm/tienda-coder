fde5b0939b3440adce1994f38b09068f65a80f21
alfonpm <alfonsina.pmq@gmail.com>
2020-12-17 13:10:48 -0300

    desafio boton

--- proyecto-ecommerce/src/components/Galeria/Grilla.jsx
+++ proyecto-ecommerce/src/components/Galeria/Grilla.jsx
@@ -1,19 +1,53 @@
 import '../Galeria/GrillaStyles.css'
+import { useState } from 'react'
 
 
-function Grilla({img ,titulo ,descrip}) {
+function Grilla({ img, titulo, precio }) {
+
+    const [cantidad, setcantidad] = useState(1);
+    const stock=20;
+    const Resta = () => {
+        if (cantidad > 1) {
+            setcantidad(cantidad - 1);
+        }
+    }
+    function Suma (){
+        if(cantidad<stock){
+        setcantidad(cantidad + 1)
+    } else{
+        alert('No hay mas stock disponible')
+    }}
+    function onAdd () {
+        if(cantidad==1)
+        alert('El producto se agrego correctamente')
+        else
+            alert(`Se agregaron ${cantidad} productos al carrito`)
+    }
+
     return (
-        
-                <article className="mx-auto text-center col-4">
-                    <figure>
-                        <img  className="img-fluid" src={img} alt=""></img>
-                    </figure>
-                    <div className="text-center">
-                        <h1>{titulo}</h1>
-                        <h2>{descrip}</h2>
-                    </div> 
-                </article>
 
-    )}
+        <article className="mx-auto text-center col-12 col-md-4">
+            <figure>
+                <img className="img-fluid" src={img} alt=""></img>
+            </figure>
+            <div className="text-center">
+                <h1>{titulo}</h1>
+                <p>{precio}</p>
+            </div>
+            <div className="cantidad text-center">
+                <input type="button" className="mas"
+                    disabled={!cantidad ? 'disabled' : null}
+                    disabled={cantidad === 1 ? 'disabled' : null}
+                    onClick={Resta} value="-"/>
+                <span className="cantProd">{cantidad}</span>
+                <input type="button" className="menos" onClick={Suma} value="+"/> 
+                <input type="button" className="agregar" onClick={onAdd} value="Agregar al carrito"/>
+                <span className='mensaje'></span>
+            </div>
+
+        </article>
+
+    )
+}
 
 export default Grilla;
